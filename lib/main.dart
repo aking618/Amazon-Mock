fc5c287b34b95f0ac40777b9d13975262a32c24d
Ayren King <ashsquads@gmail.com>
2022-03-27 13:01:34 -0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:amazon_mock/pages/home/home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Amazon Mock',
+      theme: ThemeData(
+          // primarySwatch: Colors.blue,
+          ),
+      home: const Home(),
+    );
+  }
+}
